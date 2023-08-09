cc1bef6f0ef08b4675b954eafcf6634790802dab
aymane el yamani <aymaneelyamani3@gmail.com>
2023-08-09 01:21:48 +0100

    test

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,5 @@ def uppercase(str):
         if ord(string) < 90 and ord(string) > 64:
             print("{}".format(chr(ord(string) - 25)))
         else:
-	    print("{}".format(string))
+	    print(f"{string}")
     print()
