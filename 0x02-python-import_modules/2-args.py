cf653536f7a42ae3735538833ed3cdc17efc94fd
aymane el yamani <aymaneelyamani3@gmail.com>
2023-08-10 21:01:45 +0100

    test

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,9 @@ if __name__ == "__main__":
     from sys import argv
 
     c = len(argv) - 1
-    if c == 1:
+    if c == 0:
+        print("0 arguments.")
+    elif c == 1:
         print("{} argument:".format(c))
     else:
         print("{} arguments:".format(c))
