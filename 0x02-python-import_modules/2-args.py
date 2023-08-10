bf5311f39e8ec39641647dbdea667448ead5a3c1
aymane el yamani <aymaneelyamani3@gmail.com>
2023-08-10 20:50:33 +0100

    test

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+    """Print the number of and list of arguments."""
+
     from sys import argv
+
     c = len(argv) - 1 
     if c == 1:
         print("{} argument:".format(c))
