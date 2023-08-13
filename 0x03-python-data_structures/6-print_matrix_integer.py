be2da77ad2567acff169d484c047cb8129fd1e9c
aymane el yamani <aymaneelyamani3@gmail.com>
2023-08-13 04:16:54 +0100

    fifth

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """function that print matrix"""
+    for row in matrix:
+        for col in row:
+            print("{}".format(col), end= " " if col != row[-1] else "\n")
