41f4aff1ccbc52e4289f74a097c91dc42cae63e8
aymane el yamani <aymaneelyamani3@gmail.com>
2023-08-13 02:02:34 +0100

    wa7ed lah

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    """function that prints all the elements in a list"""
+    for member in my_list:
+        print("{}".format(member))
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,2 @@
+this is basically about lists and tuples in python
+
