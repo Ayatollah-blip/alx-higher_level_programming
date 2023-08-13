b8d179a12a43d98364163659d5ec99d2de40ad58
aymane el yamani <aymaneelyamani3@gmail.com>
2023-08-13 02:11:16 +0100

    wa7ed lah

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,4 +2,4 @@
 def print_list_integer(my_list=[]):
     """function that prints all the elements in a list"""
     for member in my_list:
-        print("{}".format(member))
+        print("{:d}".format(member))
