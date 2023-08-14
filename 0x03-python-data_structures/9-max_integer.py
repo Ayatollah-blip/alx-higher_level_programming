da4a920feff5414b6dbaab0d1781ea047d73fdf6
aymane el yamani <aymaneelyamani3@gmail.com>
2023-08-14 18:37:31 +0100

    tuple

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     """function that returns bigger int in a list"""
-    if my_list  == []:
+    if my_list == []:
         return None
     max_ = my_list[0]
     for ls in my_list:
