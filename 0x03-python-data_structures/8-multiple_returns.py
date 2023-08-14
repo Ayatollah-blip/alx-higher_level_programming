a831c30a0609a8813ade6b97f8700a4aed7c0724
aymane el yamani <aymaneelyamani3@gmail.com>
2023-08-14 18:16:40 +0100

    tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,8 @@ def multiple_returns(sentence):
     """function that return tuple of length and first char"""
     ls1 = list(sentence)
     length = len(sentence)
-    new_tuple = (length, ls1[0])
+    if length == 0:
+        new_tuple = (length, None)
+    else:
+        new_tuple = (length, ls1[0])
     return new_tuple
