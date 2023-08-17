7ba700deb98f67f8fa4c54383b54375ae97c099f
aymane el yamani <aymaneelyamani3@gmail.com>
2023-08-17 01:40:03 +0100

    uniq sum

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    new_matrix = list(map(lambda x: replace if x == search else x, my_list))
-    return (new_matrix)
+def uniq_add(my_list=[]):
+    return sum(set(my_list))
