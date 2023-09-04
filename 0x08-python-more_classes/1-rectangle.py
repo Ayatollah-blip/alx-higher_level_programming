1cddbd23010d3a1685ca0238c001dd1fd0618c11
aymane el yamani <aymaneelyamani3@gmail.com>
2023-09-04 19:35:04 +0100

    test

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,7 +6,7 @@
 class Rectangle:
     """Represent a rectangle"""
 
-    def __init__(self, width = 0, height = 0):
+    def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
 
         Args:
@@ -20,6 +20,7 @@ class Rectangle:
     def height(self):
         """ height property get/set"""
         return self.__height
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -27,16 +28,16 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
     @property
     def width(self):
         """ height property get/set """
         return self.__width
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
-
-          
+        self.__width = value 
