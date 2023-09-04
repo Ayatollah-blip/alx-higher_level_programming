c58440279fa0315336f0dd49da586e165f3e39bf
aymane el yamani <aymaneelyamani3@gmail.com>
2023-09-04 19:11:12 +0100

    test

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,28 +5,33 @@
 
 class Rectangle:
     """Represent a rectangle"""
-    def __init__(self, widt, height):
-        self.__width = widt
+    """ __init__ method   """
+    def __init__(self, width = 0, height = 0):
+        self.__width = width
         self.__height = height
+    """ height property """
     @property
     def height(self):
         return __height
+    """ height setter  """
     @height.setter
-    def height(self, heit):
-        if not isinstance(heit, int):
+    def height(self, value):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif heit < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = heit
+        self.__height = value
+    """ height property  """
     @property
     def width(self):
         return __width
+    """ height setter  """
     @width.setter
-    def width(self, widt):
-        if  not isinstance(widt, int):
+    def width(self, value):
+        if  not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif widt < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = widt
+        self.__width = value
 
           
