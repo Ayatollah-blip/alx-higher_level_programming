59e5c3058f6981593c49af8d29de683f99ff431d
aymane el yamani <aymaneelyamani3@gmail.com>
2023-09-11 22:21:39 +0100

    test123

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+function that checks if the object is exactly an
+instance of the specified class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ return if it-s the same else false
+     Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is exactly an instance of a_class - True.
+        Otherwise - False.
+    """
+
+    if isinstance(obj, a_class):
+        return True
+    return False
