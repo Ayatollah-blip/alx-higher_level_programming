498647943c2899f718b5036b590578458b0d41dc
aymane el yamani <aymaneelyamani3@gmail.com>
2023-09-11 21:43:54 +0100

    2 in 1

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 def lookup(obj):
+    """ function lookup """
     return dir(obj)
--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""
+contains the MyList class
+"""
 
 class MyList(list):
+    """a subclass of list"""
     def print_sorted(self):
+        """prints the sorted list"""
         print(sorted(self))
