48f46cc4913e4bd78ad74cf48d53297d0399b67b
aymane el yamani <aymaneelyamani3@gmail.com>
2023-09-11 22:57:21 +0100

    test exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+
+contains the BaseGeometry class
+
+"""
+
+
+class BaseGeometry():
+    """a subclass of BaseGeometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
