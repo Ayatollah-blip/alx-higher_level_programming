a9e7267c7f6c63d16eb4fa71e6345ec51208519c
aymane el yamani <aymaneelyamani3@gmail.com>
2023-09-13 00:54:07 +0100

    test

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,10 +2,10 @@
 """ Reading a file"""
 
 
-def write_file(filename="", text = ""):
-    """ this function for reading file 
+def write_file(filename="", text=""):
+    """ this function for reading file
         Args:
         filename: the file to read
     """
-    with open(filename, mode='r', encoding="utf-8") as m:
-       print(m.read(), end='')    
+    with open(filename, mode='w', encoding="utf-8") as m:
+        return m.write(text)
