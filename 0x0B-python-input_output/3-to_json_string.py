b48f697f5ac915b5dcff679976d7cc4f92105393
aymane el yamani <aymaneelyamani3@gmail.com>
2023-09-13 01:19:10 +0100

    test

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-
-import json
 """ function that returns the JSON representation of an object (string):  """
+import json
 
 
 def to_json_string(my_obj):
     """  JSON REPRESENTATION OF STRING
     Args:
-        my_obj:class
-    Return:
+        my_obj:python obj
+    Returns:
           JSON REPRESENTATION
     """
     return json.dumps(my_obj)
