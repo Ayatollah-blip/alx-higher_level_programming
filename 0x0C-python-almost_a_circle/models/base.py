776e906827297233918387ebce6d769f939ed1ba
aymane el yamani <aymaneelyamani3@gmail.com>
2023-09-14 21:12:45 +0100

    test

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" the is the base classe  """
+
+class Base:
+
+    """ clase base  """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ function init
+        Args:
+            self:initiation of the class
+            id:id of the instance
+        """
+        if id is not None:
+            self.id=id
+        else:
+            Base.__nb_objects +=1
+            self.id=Base.__nb_objects
