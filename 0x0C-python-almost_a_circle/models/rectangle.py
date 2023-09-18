5dc121af84096cd50d5cbf642a2c2435be91ed1a
aymane el yamani <aymaneelyamani3@gmail.com>
2023-09-18 19:17:28 +0100

    test

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,8 +4,19 @@ from models.base import Base
 
 
 class Rectangle(Base):
-
+    """ A rectangle class"""
     def __init__(self, width, height, x=0, y=0, id=None):
+        """ The condtructor
+        Args:
+            width - The width of the class
+            height - The height of the class
+            x - the x coordinate
+            y - The y coordinate
+        Raises:
+            TypeError: if the input is not integer
+            ValueError: if width and height values are under or equal 0
+            ValueError: if x and y are under 0
+        """
         super().__init__(id)
         self.__width = width
         self.__height = height
@@ -14,10 +25,18 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """ The getter of the private attribute width
+        Returns:
+            The width value
+        """
         return self.__width
 
     @width.setter
     def width(self, width):
+        """The setter for the private attribute __width
+        Args:
+            width - the width value
+        """
         if type(width) is not int:
             raise TypeError("width must be an integer")
         if width <= 0:
@@ -26,10 +45,18 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """The getter of the private attribute height
+        Returns:
+            The height value
+        """
         return self.__width
 
     @height.setter
     def height(self, height):
+        """The setter for the private attribute __height
+        Args:
+            height - the height value
+        """
         if type(height) is not int:
             raise TypeError("height must be an integer")
         if height <= 0:
@@ -38,10 +65,18 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """ The getter of the private attribute x
+        Returns:
+            The x value
+        """
         return self.__x
 
     @x.setter
     def x(self, x):
+        """The setter for the private attribute __x
+        Args:
+            x - the x value
+        """
         if type(x) is not int:
             raise TypeError("x must be an integer")
         if x <= 0:
@@ -50,10 +85,18 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """The getter of the private attribute y
+        Returns:
+            The y value
+        """
         return self.__y
 
     @y.setter
     def y(self, y):
+        """The setter for the private attribute __y
+        Args:
+            y - the y value
+        """
         if type(y) is not int:
             raise TypeError("y must be an integer")
         if y <= 0:
