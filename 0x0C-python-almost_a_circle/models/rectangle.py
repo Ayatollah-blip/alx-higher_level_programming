b356ea1596b10e60660dd03e6cd6da13eef157a5
aymane el yamani <aymaneelyamani3@gmail.com>
2023-09-18 20:44:52 +0100

    test

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -119,3 +119,6 @@ class Rectangle(Base):
             for j in range(self.__width):
                 print("#", end="")
             print()
+    def __str__(self):
+        """ ___str__ overrided"""
+        return "[{}] ({:d}) {:d}/{:d} - {:d}/{:d}".format(type(self).__name__, self.id, self.__x, self.__y, self.__width, self.__height)
