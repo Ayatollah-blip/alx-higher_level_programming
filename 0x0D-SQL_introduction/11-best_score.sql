a9fbef0c1c72c21eb2d6dc5852b1455dc7f0cc77
Aymane El yamani <aymaneelyamani3@gmail.com>
2023-10-17 21:35:23 +0100

    test

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,4 @@
 -- A full creation of a new table with values'--
-SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
+UPDATE second_table
+SET score = 10
+WHERE name = "Bob";
