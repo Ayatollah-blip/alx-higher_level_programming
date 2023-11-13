957481f83ae7390431f53b6359f885b9d53497ad
Aymane El yamani <aymaneelyamani3@gmail.com>
2023-11-13 22:01:58 +0000

    test

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let fArray = ['C is fun', 'Python is cool', 'JavaScript is amazing']
-for(let value of fArray){
+for(let value of ['C is fun', 'Python is cool', 'JavaScript is amazing'])
+{
 console.log(value);
 }
