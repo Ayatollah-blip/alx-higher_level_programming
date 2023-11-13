bf9c224da4c22119d1d5b1ad7a3c7409458d6621
Aymane El yamani <aymaneelyamani3@gmail.com>
2023-11-13 21:00:24 +0000

    test

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+let x = process.argv.length;
+if(x === 2)
+{
+console.log('No argument');
+}
+else if(x === 3)
+{
+console.log('Argument found');
+}
+else
+{
+console.log('Arguments found');
+}
