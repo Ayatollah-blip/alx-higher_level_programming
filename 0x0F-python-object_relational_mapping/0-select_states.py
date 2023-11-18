a230fd7bae851d45a84caf7abb036b87afedeabd
Aymane El yamani <aymaneelyamani3@gmail.com>
2023-11-18 23:40:44 +0000

    test

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 
 This script lists all states from the
@@ -10,11 +11,19 @@ database `hbtn_0e_0_usa`.
 import MySQLdb
 from sys import argv
 
+
 def makingConnection(userName, password, name):
-    return MySQLdb.connect(host = "localhost", user=userName, port = 3306, passwd=password, db=name)
+    return MySQLdb.connect(
+        host="localhost",
+        user=userName,
+        port=3306,
+        passwd=password,
+        db=name)
+
 
 if __name__ == '__main__':
-    """ 
+
+    """
     Access to the database and get the states
     from the database.
     """
