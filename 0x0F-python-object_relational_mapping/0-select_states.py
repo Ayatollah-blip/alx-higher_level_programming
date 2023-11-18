8a7140e8f30a54c2b7db8d8ec9c1aedd31992b24
Aymane El yamani <aymaneelyamani3@gmail.com>
2023-11-18 15:48:49 +0000

    test

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -14,6 +14,10 @@ def makingConnection(userName, password, name):
     return MySQLdb.connect(host = "localhost", user=userName, port = 3306, passwd=password, db=name)
 
 if __name__ == '__main__':
+    """ 
+    Access to the database and get the states
+    from the database.
+    """
     db = makingConnection(argv[1], argv[2], argv[3])
     cur = db.cursor()
     cur.execute("SELECT * from states ORDER BY id ASC")
