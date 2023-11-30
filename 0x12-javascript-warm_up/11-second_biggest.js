93f12345a2c6b1989faca0fa40e6b871b91f3854
Aymane El yamani <aymaneelyamani3@gmail.com>
2023-11-30 21:44:42 +0000

    test

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const y = process.argv.length;
+let i = 0;
+if (y === 2 || y === 3){
+console.log('0');
+} else{
+while(process.argv[i] != undefined)
+{
+	console.log('test');
+	i++;
+}
+
+
+
+
