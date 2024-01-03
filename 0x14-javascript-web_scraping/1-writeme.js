174c8023fe00ec81ee08ca616a107ec18ba30922
Aymane El yamani <aymaneelyamani3@gmail.com>
2024-01-03 23:04:30 +0000

    test

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,13 +2,13 @@
 
 const fs = require('fs');
 
-const filePath = process.argv[2];
+const filePath = process.argv;
 
 if (!filePath) {
   process.exit(1);
 }
 
-fs.writeFile(filePath, 'utf8', (err, data) => {
+fs.writeFile(filePath[2], filePath[3], 'utf8', (err, data) => {
   if (err) {
     console.error(err);
   }
