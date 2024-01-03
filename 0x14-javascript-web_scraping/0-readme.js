63f1cfd91009aead6347e765d1bdf900d478841a
Aymane El yamani <aymaneelyamani3@gmail.com>
2024-01-03 22:52:23 +0000

    first

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+if (!filePath){
+	process.exit(1);
+}
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+	if (err){
+		console.error(err);
+		return;
+	}
+	console.log(data);
+})
